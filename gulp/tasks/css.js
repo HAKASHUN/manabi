966b99d85cc90478fe757addbc873c5d70b4a8e4
袴田 俊輔 <hakamata_shunsuke@cyberagent.co.jp>
2015-10-29 12:00:06 +0900

    update wrecker tasks

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -4,7 +4,6 @@ var gulp = require('gulp');
 var cssnext = require("gulp-cssnext");
 
 var config = require('../config');
-var del = require('del');
 
 gulp.task('css', function() {
   var cssConfig = config.css;
